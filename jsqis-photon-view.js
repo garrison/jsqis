984b8152b14f3deafb14f4f0be87a2e1513dfb92
Jim Garrison <jim@garrison.cc>
2013-05-28 18:56:42 -0700

    use requestAnimationFrame

--- jsqis-photon-view.js
+++ jsqis-photon-view.js
@@ -2,6 +2,7 @@
 //
 // depends: jQuery
 // depends: raphael
+// depends: requestAnimationFrame browser support || rAF.js
 
 jQuery.extend(window.jsqis, (function ($, Raphael) {
 
@@ -13,7 +14,7 @@ jQuery.extend(window.jsqis, (function ($, Raphael) {
         this.paper = Raphael(this.elt[0], 2 * r1, 2 * r1);
         this.paper.circle(r1, r1, r2 / 2).attr({fill: "#aaa", stroke: "none"});
         this.circle = this.paper.circle(r1, r1, r2).attr({fill: "#00a", stroke: "#00e"});
-        this.t = 0;
+        this.originTime = new Date().getTime();
         this.r3 = r1 - r2;
         this.update(machine);
         this.render();
@@ -28,26 +29,27 @@ jQuery.extend(window.jsqis, (function ($, Raphael) {
         this.phase1 = machine.amplitudeList[0].arg();
         this.phase2 = machine.amplitudeList[1].arg();
     };
-    PhotonView.prototype.render = function () {
-        var x = this.amplitude2 * Math.sin(this.t + this.phase2),
-            y = this.amplitude1 * Math.sin(this.t + this.phase1);
+    PhotonView.prototype.render = function (t) {
+        var x = this.amplitude2 * Math.sin(t + this.phase2),
+            y = this.amplitude1 * Math.sin(t + this.phase1);
         this.circle.transform("T" + (x * this.r3) + "," + (-y * this.r3));
     };
     PhotonView.prototype.toggleAnimation = function (status) {
         var self = this;
+        var animateFrame = function (time) {
+            self.render((time - self.originTime) / 200);
+            self.intervalID = window.requestAnimationFrame(animateFrame);
+        };
         if (status === undefined) {
             // if status is not given, switch it.
             status = (this.intervalID === undefined);
         }
         if (status && this.intervalID === undefined) {
             // start animation
-            this.intervalID = window.setInterval(function () {
-                self.t += .05;
-                self.render();
-            }, 10);
+            this.intervalID = window.requestAnimationFrame(animateFrame);
         } else if (!status && this.intervalID !== undefined) {
             // stop animation
-            window.clearInterval(this.intervalID);
+            window.cancelAnimationFrame(this.intervalID);
             this.intervalID = undefined;
         }
     };
